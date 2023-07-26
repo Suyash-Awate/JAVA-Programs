97c210486b69a7d2293f0aab01ed4e6179114c5f
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-07-26 14:25:53 +0530

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,23 @@
+package com.operator;
+
+public class RelationalOperator
+{
+	public static void main(String [] args)
+	{
+		int a = 10;
+		int b = 20;
+		
+		boolean b1 = a<b;
+		System.out.println(b1);
+		
+		boolean b2 = a>b;
+		System.out.println(b2);
+		
+		int c = 20;
+		boolean b3 = c<=b;
+		System.out.println(b3);
+		
+		boolean b4 = c==b;
+		System.out.println(b4);
+	}
+}
