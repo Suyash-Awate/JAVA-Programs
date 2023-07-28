db2f60b2c710ef135ccacf7f1ed7482ca587da22
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-07-28 15:43:15 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,22 @@
+package com.operator;
+
+public class Test {
+	public static void main(String [] args) {
+		byte num1; //variable declaration	
+		short num2 = 10; //variable intialization
+		int num3 = 24;
+		long num4 = 45;
+		float num5 = 34.5f;
+		double num6 = 78;
+		char ch = 'a';
+		
+		System.out.println("Value of num2 -> "+num2);
+		System.out.println("Value of num3 -> "+num3);
+		System.out.println("Value of num4 -> "+num4);
+		System.out.println("Value of num5 -> "+num5);
+		System.out.println("Value of num6 -> "+num6);
+		System.out.println("Value of ch -> "+ch);
+		
+	}
+
+}
