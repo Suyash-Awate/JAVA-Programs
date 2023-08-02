8981c51d6a724c55f348a26a4c590872a4c2ed14
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-08-02 14:45:55 +0530

    Add files via upload

---
+++ Operators/ArithmaticOperators.java
@@ -0,0 +1,22 @@
+package com.operator;
+
+public class ArithmaticOperators
+{
+	public static void main(String [] args)
+	{
+		int a = 10;
+		int b = 20;
+		int add = a+b;
+		int sub = a-b;
+		int mul = a*b;
+		int div = a/b;
+		int mod = a%b;
+		
+		System.out.println("Addtion is -> "+ add);
+		System.out.println("Subtraction is -> "+ sub);
+		System.out.println("Multiplication is -> "+ mul);
+		System.out.println("Division is -> "+ div);
+		System.out.println("Remainder is -> "+ mod);
+
+	}
+}
