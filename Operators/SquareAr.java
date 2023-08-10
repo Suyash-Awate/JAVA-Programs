5f05fa87892f646e8b2caf4cabd5da12b5f3a4c1
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-08-10 14:48:02 +0530

    Add files via upload

---
+++ Operators/SquareAr.java
@@ -0,0 +1,13 @@
+package com.operator;
+
+public class SquareAr 
+{
+	public static void main(String[] args) 
+	{
+		float a = 10.3f;
+		float area = a*a;
+		
+		System.out.println("Area of square is : "+area);
+	}
+
+}
