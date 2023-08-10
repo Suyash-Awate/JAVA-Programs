69276bc967c2a6c4cbf5d168f6ec3966a60dad36
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-08-10 14:52:31 +0530

    Add files via upload

---
+++ Control Statements/OddNo.java
@@ -0,0 +1,14 @@
+package com.ControlStatement;
+
+public class OddNo
+{
+	public static void main(String[] args) 
+	{
+		int iNum = 7;
+		
+		if(iNum%2 != 0)
+		{
+			System.out.println("It is Odd number");
+		}
+	}
+}
