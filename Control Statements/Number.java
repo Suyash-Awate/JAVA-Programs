3861942e20bc3166ebe8dd2edc60d2868536e870
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-08-10 14:52:53 +0530

    Add files via upload

---
+++ Control Statements/Number.java
@@ -0,0 +1,22 @@
+package com.ControlStatement;
+
+public class Number 
+{
+	public static void main(String[] args) 
+	{
+		int iNum = 5;
+		
+		if(iNum > 0)
+		{
+			System.out.println("Positive number");
+		}
+		else if(iNum == 0)
+		{
+			System.out.println("Number equal t zero");
+		}
+		else
+		{
+			System.out.println("Negative number");
+		}
+	}
+}
