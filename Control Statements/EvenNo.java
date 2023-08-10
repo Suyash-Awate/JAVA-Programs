dda5b3b216990edbd1ebfde371e33df35a728520
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-08-10 14:54:31 +0530

    Add files via upload

---
+++ Control Statements/EvenNo.java
@@ -0,0 +1,14 @@
+package com.ControlStatement;
+
+public class EvenNo 
+{
+	public static void main(String[] args) 
+	{
+		int iNum = 10;
+		
+		if(iNum%2 == 0)
+		{
+			System.out.println("It is even");
+		}
+	}
+}
