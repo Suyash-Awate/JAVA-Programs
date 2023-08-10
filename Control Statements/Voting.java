1a42559afa32a6ca4fbe4826c9c780cea845c43f
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-08-10 14:50:04 +0530

    Add files via upload

---
+++ Control Statements/Voting.java
@@ -0,0 +1,18 @@
+package com.ControlStatement;
+
+public class Voting
+{
+	public static void main(String[] args) 
+	{
+		int age = 15;
+	
+		if(age > 18)
+		{
+			System.out.println("Eligible for voting");
+		}
+		else
+		{
+			System.out.println("Not Eligible for voiting");
+		}
+	}
+}
