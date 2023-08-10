e107ee487a6a618caf9ead7323ea81e941dadd02
Suyash Sadashiv Awate <101892435+Suyash-Awate@users.noreply.github.com>
2023-08-10 14:51:28 +0530

    Add files via upload

---
+++ Control Statements/Student.java
@@ -0,0 +1,18 @@
+package com.ControlStatement;
+
+public class Student 
+{
+	public static void main(String[] args) 
+	{
+		float per = 47.8f;
+		
+		if(per > 35)
+		{
+			System.out.println("Pass");
+		}
+		else
+		{
+			System.out.println("Fail");
+		}
+	}
+}
